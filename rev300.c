e851025734076dca57be77e4d9745f38035d4703
Bryce Kerley <bkerley@brycekerley.net>
2008-05-31 05:27:28 -0400

    runs, not sure wtf

--- rev300.c
+++ rev300.c
@@ -5,10 +5,10 @@ int main() {
 	char* fart;
 	char* flat;
 	int count;
-	fart = strdup("fartface");
-	flat = strdup("fartfart");
+	fart = strdup("asdf");
+	flat = strdup("zdddasdfasdf");
 	
-	printf("\nit begins\n");
+	printf("\nit begins\nfart %p %s\nflat %p %s\n",fart, fart, flat, flat);
 	
 	__asm__(
 	"dhoom: \n"
@@ -17,27 +17,25 @@ int main() {
 	"	jmp anus \n"
 	"poopie: \n"
 	"	call	sub_16                \n"
-	"	movl	%eax,%edi                   \n"
-	"	xorl	%eax,%eax                   \n"
-	"	orl	$0xffffffff, %ecx            \n"
+	"	mov	%eax,%edi                   \n"
+	"	xor	%eax,%eax                   \n"
+	"	or	$0xffffffff, %ecx            \n"
 	"	repne/scasb         \n"
-	"	negl	%ecx                        \n"
-	"	movl	%ecx,%edx                   \n"
+	"	neg	%ecx                        \n"
+	"	mov	%ecx,%edx                   \n"
 	"	ret \n"
 	"sub_16: \n"
-	"	xorl	%eax,%eax                   \n"
-	"	pushl	%ebx                      \n"
-	"	movl	$0x00000001, %ebx            \n"
+	"	xor	%eax,%eax                   \n"
+	"	push	%ebx                      \n"
+	"	mov	$0x00000001, %ebx            \n"
 	"here: \n"
-	"	lock/cmpxchgl	%ebx,	-44(%esp)   \n"
+	"	lock/cmpxchg	%ebx,	-44(%esp)   \n"
 	"	jnz	here                  \n"
-	"	popl	%ebx                        \n"
+	"	pop	%ebx                        \n"
 	"	ret                              \n"
 	"	lock/movb	$0x00,	-44(%esp)      \n"
 	"	ret                              \n"
-	"	.byte	0,0,0,0,0                  \n"
-	"sphincter: \n"
-	"	.quad	0\n"
+	"	.byte	0,0,0,0,0,0                \n"
 	"anus: \n"
 	"	nop \n"
 	);
